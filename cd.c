5be48c7a51652a9f86ca55cba778f34578e043ed
Maxwellowuor254 <122903684+Maxwellowuor254@users.noreply.github.com>
2023-05-27 02:45:29 +0300

    Update cd.c

--- cd.c
+++ cd.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * cd_shell - changes current directory
+ * cd_shell - changing current dir
  *
- * @datash: data relevant
+ * @datash: data 
  * Return: 1 on success
  */
 int cd_shell(data_shell *datash)
