ef548c469837e4a4bb6665edeb6b7709b6274687
Maxwellowuor254 <122903684+Maxwellowuor254@users.noreply.github.com>
2023-05-27 03:54:57 +0300

    Update strings3.c

--- strings3.c
+++ strings3.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string.
- * @s: input string.
+ * rev_string - reversing a str.
+ * @s: input str.
  * Return: no return.
  */
 void rev_string(char *s)
