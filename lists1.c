6d76fdf00e20dcd84deae37b02ee048aa670912a
Maxwellowuor254 <122903684+Maxwellowuor254@users.noreply.github.com>
2023-05-27 03:27:55 +0300

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * add_sep_node_end - adds a separator found at the end
- * of a sep_list.
+ * add_sep_node_end - adds a separatig found at the end
  * @head: head of the linked list.
  * @sep: separator found (; | &).
  * Return: address of the head.
@@ -34,7 +33,7 @@ sep_list *add_sep_node_end(sep_list **head, char sep)
 }
 
 /**
- * free_sep_list - frees a sep_list
+ * free_sep_list - freeing a separate_list
  * @head: head of the linked list.
  * Return: no return.
  */
@@ -56,7 +55,7 @@ void free_sep_list(sep_list **head)
 }
 
 /**
- * add_line_node_end - adds a command line at the end
+ * add_line_node_end - adding a cmmnd line at the end
  * of a line_list.
  * @head: head of the linked list.
  * @line: command line.
